038a4ef4dc023da655fe5f82efb56fb68f52eba4
maikeduarte <67766694+maikeduarte@users.noreply.github.com>
2020-08-02 18:52:49 -0300

    Add files via upload

---
+++ dados.php
@@ -0,0 +1,28 @@
+<?php
+$titulo="Usando PHP";
+$subtitulo="Primeira Tela PHP+HTML";
+$estilo = ['Samba', 'Pop Rock'];
+
+
+
+$albuns=[
+    [
+        'titulo' => 'Skank',
+        'descricao'=>'Album 2009',
+        'capa'=>'',
+        'duracao'=>'8 min',
+    ],
+    [
+    'titulo' => 'Titãn',
+    'descricao'=>'Album 1999',
+    'capa'=>'',
+    'duracao'=>'18 min',
+],
+[
+'titulo' => 'Zeca Pagodinho',
+        'descricao'=>'Deixa a Vida me Levar',
+        'capa'=>'',
+        'duracao'=>'28 min',
+    ],
+]
+?>
